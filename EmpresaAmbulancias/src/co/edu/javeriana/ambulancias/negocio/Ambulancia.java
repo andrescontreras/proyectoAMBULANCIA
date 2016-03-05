87f6698ebf822e6d4e9ce1c4733e3f988a5b474b
andre <andre@DESKTOP-N2P91H5>
2016-03-04 22:00:18 -0500

    partes basicas de clase ambulancia

--- EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/negocio/Ambulancia.java
+++ EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/negocio/Ambulancia.java
@@ -1,10 +1,163 @@
 package co.edu.javeriana.ambulancias.negocio;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import co.edu.javeriana.ambulancias.presentacion.Utils;
+
 public class Ambulancia {
+	/***
+	 * --la clase es una agregacion de la clase empresa ambulancias
+	 * --tiene una relacion con servicios de una ambulancia 
+	 * 	 tiene uno o mas servicios
+	 * */
 	private int codigo;
 	private String placa;
 	private String tipoDotacion;
-	private date horaPosicion;
+	private GregorianCalendar horaPosicion;
 	private int posicionCalle;
 	private int posicionCarrera;
+	
+	private List<Servicio> servicios = new ArrayList<Servicio>();
+// ---- CONSTRUCTORES
+
+	public Ambulancia(int codigo, String placa, String tipoDotacion, GregorianCalendar horaPosicion, int posicionCalle,
+			int posicionCarrera) {
+		super();
+		this.codigo = codigo;
+		this.placa = placa;
+		this.tipoDotacion = tipoDotacion;
+		this.horaPosicion = horaPosicion;
+		this.posicionCalle = posicionCalle;
+		this.posicionCarrera = posicionCarrera;
+	}
+	
+	public Ambulancia(){
+		super();
+		this.codigo=0;
+		this.placa="-----";
+		this.tipoDotacion="-----";
+		this.horaPosicion = new GregorianCalendar();
+		this.posicionCalle=0;
+		this.posicionCarrera=0;
+	}
+
+	// ---- SET Y GET
+	
+	/**
+	 * @return the codigo
+	 */
+	public int getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	/**
+	 * @return the placa
+	 */
+	public String getPlaca() {
+		return placa;
+	}
+
+	/**
+	 * @param placa the placa to set
+	 */
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	/**
+	 * @return the tipoDotacion
+	 */
+	public String getTipoDotacion() {
+		return tipoDotacion;
+	}
+
+	/**
+	 * @param tipoDotacion the tipoDotacion to set
+	 */
+	public void setTipoDotacion(String tipoDotacion) {
+		this.tipoDotacion = tipoDotacion;
+	}
+
+	/**
+	 * @return the horaPosicion
+	 */
+	public GregorianCalendar getHoraPosicion() {
+		return horaPosicion;
+	}
+
+	/**
+	 * @param horaPosicion the horaPosicion to set
+	 */
+	public void setHoraPosicion(GregorianCalendar horaPosicion) {
+		this.horaPosicion = horaPosicion;
+	}
+
+	/**
+	 * @return the posicionCalle
+	 */
+	public int getPosicionCalle() {
+		return posicionCalle;
+	}
+
+	/**
+	 * @param posicionCalle the posicionCalle to set
+	 */
+	public void setPosicionCalle(int posicionCalle) {
+		this.posicionCalle = posicionCalle;
+	}
+
+	/**
+	 * @return the posicionCarrera
+	 */
+	public int getPosicionCarrera() {
+		return posicionCarrera;
+	}
+
+	/**
+	 * @param posicionCarrera the posicionCarrera to set
+	 */
+	public void setPosicionCarrera(int posicionCarrera) {
+		this.posicionCarrera = posicionCarrera;
+	}
+
+	/**
+	 * @return the servicios
+	 */
+	public List<Servicio> getServicios() {
+		return servicios;
+	}
+
+	/**
+	 * @param servicios the servicios to set
+	 */
+	public void setServicios(List<Servicio> servicios) {
+		this.servicios = servicios;
+		
+	}
+	// ---- TO STRING
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		String hora=Utils.convertirHoraString(horaPosicion);
+		return String.format("%10d %10s %14s %15s %5d %5d ",codigo,posicionCalle,placa,tipoDotacion,posicionCalle,posicionCarrera);
+	}
+	
+// ----  METODOS EXTRA
+	public void agregarServicio(){
+		
+	}
+
+	
+	
 }
--- EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/presentacion/Utils.java
+++ EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/presentacion/Utils.java
@@ -1,12 +1,96 @@
-/**
- * 
- */
-package co.edu.javeriana.ambulancias.presentacion;
+package co.edu.javeriana.empleados.presentacion;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import  java.util.StringTokenizer;
+
 
 /**
- * @author andre
+ * @author sala_c
  *
  */
 public class Utils {
+	/**
+ 	* Metodo encargado de convertir una variable de tipo String a GregorianCalendar (en formato anio,mes,dia )
+ 	*/
+	public static GregorianCalendar convertirStringFecha (String fecha)
+	{
+		String dia,mes,anio;
+		int diai,mesi,anioi;
+		StringTokenizer st = new StringTokenizer(fecha,"-");
 
+	         anio=st.nextToken();
+	         mes=st.nextToken();
+	         dia=st.nextToken();
+	         anioi = Integer.parseInt(anio);
+	         mesi = Integer.parseInt(mes);
+	         diai = Integer.parseInt(dia);
+	         GregorianCalendar fechaHoy = new GregorianCalendar(anioi,mesi,diai);
+	         return fechaHoy;
+	}
+	/**
+ 	* Metodo encargado de convfertir una variable de tipo GregorianCalendar a String (en formato anio,mes,dia)
+ 	*/
+	public static String convertirFechaString (GregorianCalendar fecha)
+	{
+		int dia = fecha.get(Calendar.DATE);
+		int mes = fecha.get(Calendar.MONTH);
+		int anio = fecha.get(Calendar.YEAR);
+		String sfecha = anio+"-"+mes+"-"+dia;
+		return sfecha;
+	}
+	/**
+ 	* Metodo encargado de cinvertir una vaiable de tipo GregorianCalendar a String (en formato hora:minutos )
+ 	*/
+	public static String convertirHoraString(GregorianCalendar hora)
+	{
+		int horas = hora.get(Calendar.HOUR);
+		int minutos = hora.get(Calendar.MINUTE);
+		String shora = horas+":"+minutos;
+		return shora;
+	}
+	/**
+ 	* Metodo encargado de convertir una variable de tipo String a GregorianCalendar (en formato hora:minutos) y agregarsela a una fecha.
+ 	*/
+	public static GregorianCalendar convertirStringHoraToFecha (String fecha)
+	{
+		StringTokenizer fh = new StringTokenizer(fecha,":");
+		String hora;
+		String minutos;
+		int ihora;
+		int iminutos;
+		
+		hora = fh.nextToken();
+		minutos = fh.nextToken();
+		
+		ihora=Integer.parseInt(hora); 
+		iminutos=Integer.parseInt(minutos); 
+		
+		GregorianCalendar fechaHoy = new GregorianCalendar();
+		
+		int dia = fechaHoy.get(Calendar.DATE);
+		int mes = fechaHoy.get(Calendar.MONTH);
+		int anio = fechaHoy.get(Calendar.MONTH);
+		return new GregorianCalendar(anio,mes,dia,
+				 ihora, iminutos);
+	}
+	/**
+ 	* Metodo encargado de calcular la edad de un operario en años a partir de su fecha de nacimiento.
+ 	*/
+	public static long obtenerEdad (GregorianCalendar fechaNacimiento)
+	{
+		GregorianCalendar fechaHoy = new GregorianCalendar();
+		long resta=fechaHoy.getTimeInMillis()-fechaNacimiento.getTimeInMillis();
+		resta=resta/((long) 1000*60*60*24*365);
+		return resta;
+	}
+	/**
+ 	* Metodo encargado de calcular la diferencia en minutoos de dos fechas.
+ 	*/
+	public static long diferenciaFechasMinutos(GregorianCalendar fecha1, GregorianCalendar
+			fecha2){
+		long resta=fecha1.getTimeInMillis()-fecha2.getTimeInMillis();
+		resta=resta/((long) 1000*60);
+		return resta;
+	}
+	
 }
