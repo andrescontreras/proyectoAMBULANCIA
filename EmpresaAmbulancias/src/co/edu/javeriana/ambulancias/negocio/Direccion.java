51a292ecba1ba1066eee6b83fd4d368b1640eb93
Juan Castañeda <juanario1997@gmail.com>
2016-03-05 18:35:15 -0500

    cambios en ervicio y direccion

--- EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/negocio/Direccion.java
+++ EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/negocio/Direccion.java
@@ -4,6 +4,37 @@ public class Direccion {
 	private String tipoDireccion;
 	private int calle;
 	private int carrera;
+	public Direccion(String tipoDireccion, int calle, int carrera, int numero) {
+		super();
+		this.tipoDireccion = tipoDireccion;
+		this.calle = calle;
+		this.carrera = carrera;
+		this.numero = numero;
+	}
+	public String getTipoDireccion() {
+		return tipoDireccion;
+	}
+	public void setTipoDireccion(String tipoDireccion) {
+		this.tipoDireccion = tipoDireccion;
+	}
+	public int getCalle() {
+		return calle;
+	}
+	public void setCalle(int calle) {
+		this.calle = calle;
+	}
+	public int getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(int carrera) {
+		this.carrera = carrera;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 	private int numero;
 
 }
--- EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/negocio/Servicio.java
+++ EmpresaAmbulancias/src/co/edu/javeriana/ambulancias/negocio/Servicio.java
@@ -3,19 +3,95 @@
  */
 package co.edu.javeriana.ambulancias.negocio;
 
+import java.util.GregorianCalendar;
+
 /**
  * @author andre
  *
  */
 public class Servicio {
-	private long CONSECUTIVO=0;
-	private date horaSolicitud;
+	private static long CONSECUTIVO=0;
+	private GregorianCalendar horaSolicitud;
 	private String paciente;
 	private String tipoServicio;
 	private String telefono;
 	private String estado;
-	public void asignarDireccion()
-	{
+	private Direccion direccion;
+	private Ambulancia ambulancia;
+	
+	public Servicio(String paciente, String tipoServicio, String telefono,String tipoDireccion,int calle,int
+			carrera,int numero) {
 		
+		this.CONSECUTIVO=CONSECUTIVO++;
+		this.paciente = paciente;
+		this.tipoServicio = tipoServicio;
+		this.telefono = telefono;
+		asignarDireccion(tipoDireccion, calle,
+				carrera, numero);
+		this.horaSolicitud=new GregorianCalendar();
+		this.estado="NO_ASIGNADO";
+		
+	}
+
+	public GregorianCalendar getHoraSolicitud() {
+		return horaSolicitud;
+	}
+
+	public void setHoraSolicitud(GregorianCalendar horaSolicitud) {
+		this.horaSolicitud = horaSolicitud;
+	}
+
+	public String getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(String paciente) {
+		this.paciente = paciente;
+	}
+
+	public String getTipoServicio() {
+		return tipoServicio;
+	}
+
+	public void setTipoServicio(String tipoServicio) {
+		this.tipoServicio = tipoServicio;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	public Ambulancia getAmbulancia() {
+		return ambulancia;
+	}
+
+	public void setAmbulancia(Ambulancia ambulancia) {
+		this.ambulancia = ambulancia;
+	}
+
+	public void asignarDireccion(String tipoDireccion,int calle,int
+			carrera,int numero)
+	{
+		this.direccion=new Direccion(tipoDireccion, calle, carrera, numero);
 	}
 }
